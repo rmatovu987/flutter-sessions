6793849495589cb73133b3d50420baa22f4be80d
matovu <matovu@finplusgroup.com>
2023-07-21 17:34:15 +0300

    Modify task 7

--- First assigment - Data types and Variables/7. extract_months_and_count_number_of_birthdays.dart
+++ First assigment - Data types and Variables/7. extract_months_and_count_number_of_birthdays.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
 
-const String FILEPATH = "birthdays.json";
+const String FILEPATH =
+    "First assigment - Data types and Variables/birthdays.json";
 const JsonDecoder decoder = JsonDecoder();
 
 void main() {
@@ -13,11 +14,95 @@ void main() {
   print(unique_keys.keys);
 
   // Count the number of birthdays per month
-  List<String> keys_and_values = data.split(' ');
+  List keys_and_values = data.split(' ');
+  keys_and_values.removeWhere((val) => val.isEmpty);
+
   for (String key in unique_keys.keys) {
     int birthdays = countOccurrencesUsingWhere(keys_and_values, "\"$key\":");
     print("$key has $birthdays birthdays");
   }
+
+  // create list with only month and day values
+  List trimmed_keys_and_values = [];
+  keys_and_values.forEach((a) {
+    var b = a.trim();
+    if (!(b.toString() == '{') && !(b.toString() == ']')) {
+      if (b.contains('}')) {
+        b = b.replaceAll('\n}', '');
+      }
+      trimmed_keys_and_values.add(
+          b.replaceAll('\"', '').replaceAll('\:', '').replaceAll('\,', ''));
+    }
+  });
+
+  // Create a list of dates
+  int i = 0;
+  List dates = [];
+  while (i <= trimmed_keys_and_values.length - 2) {
+    var a = trimmed_keys_and_values[i];
+    var b = trimmed_keys_and_values[i + 1];
+    dates.add("$a $b");
+    i = i + 2;
+  }
+
+  //Get highest month
+  int highest = 0;
+  int highest_day = 32;
+  int lowest = 12;
+  int lowest_day = 0;
+  String highest_month = "";
+  String lowest_month = "";
+  dates.forEach((element) {
+    String month =
+        element.replaceAll(new RegExp('[0-9]'), "").replaceAll(" ", "").trim();
+
+    int day = int.parse(
+        element.replaceAll(new RegExp(r"\D"), "").replaceAll(" ", "").trim());
+
+    int month_int = 0;
+    switch (month) {
+      case "May":
+        month_int = 3;
+        break;
+
+      case "November":
+        month_int = 10;
+        break;
+
+      case "December":
+        month_int = 11;
+        break;
+
+      default:
+        print("Invalid choice");
+        break;
+    }
+
+    if (month_int > highest) {
+      highest_month = element;
+      highest = month_int;
+      highest_day = day;
+    } else if (month_int == highest) {
+      if (highest_day < day) {
+        highest_month = element;
+        highest_day = day;
+      }
+    }
+
+    if (month_int < lowest) {
+      lowest_month = element;
+      lowest = month_int;
+      lowest_day = day;
+    } else if (month_int == lowest) {
+      if (day < lowest_day) {
+        lowest_month = element;
+        lowest_day = day;
+      }
+    }
+  });
+
+  print("The highest birthday is $highest_month");
+  print("The lowest birthday is $lowest_month");
 }
 
 String getJsonFile() {
@@ -26,6 +111,6 @@ String getJsonFile() {
   return json.decode(data);
 }
 
-int countOccurrencesUsingWhere(List<String> values, String element) {
+int countOccurrencesUsingWhere(List values, String element) {
   return values.where((e) => e == element).length;
 }
